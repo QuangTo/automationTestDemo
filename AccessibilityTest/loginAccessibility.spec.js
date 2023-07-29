5cc5998cb5d034b7519ec99c1404bb7f2a91e190
QuangToo <quangtoo@QuangToos-MacBook-Pro.local>
2023-07-29 09:25:16 +0700

    save a11y work

--- AccessibilityTest/loginAccessibility.spec.js
+++ AccessibilityTest/loginAccessibility.spec.js
@@ -1,12 +1,8 @@
 import { t } from 'testcafe'
 import { Accessibility } from "./util";
 
-const METRICS = ( 
-    'Critical',  'serious', 'moderate', 'minor'
-);
-
 fixture('Test Accessibility')
-    .page(`https://react-redux.realworld.io/#/login`);
+    .page(`https://google.com/`);
 test('Verify login page', async () => {
     await Accessibility.scanPage(t, 'Login');
 })
--- AccessibilityTest/util.js
+++ AccessibilityTest/util.js
@@ -7,17 +7,24 @@
 import { axeCheck } from '@testcafe-community/axe';
 import { createHtmlReport } from 'axe-html-reporter';
 
+
 export const Accessibility = {
 
-    scanPage: async (t, pageName, metrics) => {
+    scanPage: async (t, pageName) => {
         // Run axe check, not using wrapper function that throws error
-        const { error, results } = await axeCheck(t); // "context" and "options" parameters are optional
+        const { error, violations } = await axeCheck(t); // "context" and "options" parameters are optional
+        const metrics = {
+            critical: 0,
+            serious: 0,
+            moderate: 0,
+            minor: 0,
+        };
 
-        if (metrics) {
-            const numCritical = results.violations.filter((r) => r.impact === "critical").length;
-            const numSerious = results.violations.filter((r) => r.impact === "serious").length;
-            const numModerate = results.violations.filter((r) => r.impact === "moderate").length;
-            const numMinor = results.violations.filter((r) => r.impact === "minor").length;
+        if (violations) {
+            const numCritical = violations.filter((r) => r.impact === "critical").length;
+            const numSerious = violations.filter((r) => r.impact === "serious").length;
+            const numModerate = violations.filter((r) => r.impact === "moderate").length;
+            const numMinor = violations.filter((r) => r.impact === "minor").length;
 
             metrics.critical += numCritical;
             metrics.serious += numSerious;
@@ -29,11 +36,11 @@ export const Accessibility = {
         await t.expect(error).eql(null, `axe check failed with an error: ${error}`);
 
         createHtmlReport({
-            results,
+            results: violations,
             options: {
-                projectKey: `"${pageName}"`,
-                outputDir: `reports/artifacts/${pageName}`,
+              projectKey: `"${pageName}"`,
+              outputDir: `reports/artifacts/${pageName}`,
             },
-        });
+          });
     },
 };
